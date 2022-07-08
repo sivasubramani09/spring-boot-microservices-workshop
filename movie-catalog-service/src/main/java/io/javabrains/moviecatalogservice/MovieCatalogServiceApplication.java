7b469e538e032dfe81ac1dbaac728171d47a08ef
sivasubramani09 <36350531+sivasubramani09@users.noreply.github.com>
2022-07-08 16:50:40 +0530

    Adding Hystrix to a Spring Boot app

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,13 +2,16 @@ package io.javabrains.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
@@ -18,6 +21,11 @@ public class MovieCatalogServiceApplication {
 	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate() {
+		/*
+		HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		httpComponentsClientHttpRequestFactory.setConnectTimeout(3000);
+		return new RestTemplate(httpComponentsClientHttpRequestFactory);
+		*/
 		return new RestTemplate();
 	}
 }
--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -27,6 +29,7 @@ public class CatalogResource {
     WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
@@ -38,6 +41,10 @@ public class CatalogResource {
                 })
                 .collect(Collectors.toList());
 
+    }
+    
+    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
+    	return Arrays.asList(new CatalogItem("No movie", "", 0));
     }
 }
 
